6b67ac0fa1dfe79259b42d659d50c90d6749dacc
michellemej22596 <mej22596@uvg.edu.gt>
2024-05-07 17:43:19 -0600

    Feat: React suspense added to load pages

--- frontend/src/pages.jsx
+++ frontend/src/pages.jsx
@@ -1,28 +1,36 @@
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import AdminHome from './pages/AdminHome';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Posts from './pages/Posts';
-import Create from './pages/Create';
-import Edit from './pages/Edit';
-import Delete from './pages/Delete';
-import Select from './pages/SelectPostToEdit';
 import PropTypes from 'prop-types';
+
+// Lazy loading de los componentes
+const Home = lazy(() => import('./pages/Home'));
+const AdminHome = lazy(() => import('./pages/AdminHome'));
+const Login = lazy(() => import('./pages/Login'));
+const Register = lazy(() => import('./pages/Register'));
+const Posts = lazy(() => import('./pages/Posts'));
+const Create = lazy(() => import('./pages/Create'));
+const Edit = lazy(() => import('./pages/Edit'));
+const Delete = lazy(() => import('./pages/Delete'));
+const Select = lazy(() => import('./pages/SelectPostToEdit'));
+
+// Un componente básico de "skeleton loading"
+const LoadingIndicator = () => <div>Cargando...</div>;
 
 function Pages({ token, setToken }) {
   return (
-    <Routes>
-      <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
-      <Route path="posts" element={<Posts />} />
-      <Route path="/login" element={<Login setToken={setToken} />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/admin" element={<AdminHome />} />
-      <Route path="/admin/create" element={<Create />} />
-      <Route path="edit/:postId" element={<Edit />} />
-      <Route path="/admin/delete" element={<Delete />} />
-      <Route path="/admin/select" element={<Select />} />
-    </Routes>
+    <Suspense fallback={<LoadingIndicator />}>
+      <Routes>
+        <Route path="/" element={token ? <Home /> : <Navigate to="/login" />} />
+        <Route path="posts" element={<Posts />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminHome />} />
+        <Route path="/admin/create" element={<Create />} />
+        <Route path="edit/:postId" element={<Edit />} />
+        <Route path="/admin/delete" element={<Delete />} />
+        <Route path="/admin/select" element={<Select />} />
+      </Routes>
+    </Suspense>
   );
 }
 
